dd559b5ea70b6cdc671ac162cbbf9e0634cb2385
Your Name <you@example.com>
2022-02-10 19:18:26 +0800

    完成个人中心功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -16,7 +16,7 @@ $(function () {
         // 通过verify函数()自定义了密码的校验规则
         pass: [
             /^[\S]{6,12}$/
-            ,'密码必须6到12位，且不能出现空格'
+            ,'密码必须6到12位,且不能出现空格'
         ],
         repass: function (val) { 
             var pass = $(".reg-box [name=password]").val();
